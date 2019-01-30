d0b05baaafcca5576c8e5cc05f3e2e6d9fed406c
Quentin Sahal <quentin.sahal@gmail.com>
2019-01-30 11:44:35 +0100

    delete commented old code

--- src/modules/Messenger/WelcomeLayout.js
+++ src/modules/Messenger/WelcomeLayout.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
 
-//import FormDialog from "../lib/Components/FormDialog";
-//import AutosuggestFormField from "../lib/Components/AutosuggestFormField"
-
 import Fab from "@material-ui/core/Fab";
 
 import "./WelcomeLayout.css";
@@ -30,32 +27,6 @@ class WelcomeLayout extends Component {
                     </div>
 
                     <Fab variant="extended" onClick={startDiscussion}>{"start discussion"}</Fab>
-
-                    {/*<FormDialog
-                        text= "To subscribe to this website, please enter your email address here. We will send updates occasionally."
-                        title="Start your conversation"
-                        triggerBtnText="new conversation"
-                        onValidateForm={startDiscussion}
-                        validateText = "validate"
-                    >
-                        <AutosuggestFormField
-                            listOfSuggestions={friendlist}
-                            nbOfSuggestions={3}
-                            getSuggestionValue={getFriendName}
-                            onHandleChange={newValue => {
-                                this.setState((state) => ({
-                                    ...state,
-                                    friendToDiscussWith: newValue
-                                }))
-                                    
-                            }}
-                            inputProps={{
-                            placeholder: "Enter a friend's name",
-                            label: ""
-                            }}
-                        />
-                        
-                        </FormDialog>*/}
                     
                 </div>
                 

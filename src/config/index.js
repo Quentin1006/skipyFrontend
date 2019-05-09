b1f85ca318731303a437a5537065014cc186faa3
Quentin Sahal <quentin.sahal@gmail.com>
2019-05-09 14:00:14 +0200

    added config, removed .env file

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+export const NOT_SENDING = 0;
+export const SENDING_MESSAGE = 1; 
+export const SENDING_SUCCESS = 2;
+export const SENDING_FAILURE = 3;
+
+export const SEND_MESSAGE_STATUS = [
+    NOT_SENDING,
+    SENDING_MESSAGE,
+    SENDING_SUCCESS,
+    SENDING_FAILURE
+]
+
+
+export const LEFT = "left";
+export const RIGHT = "right";
+
---
+++ src/config/server.js
@@ -0,0 +1,7 @@
+export default {
+    url: "https://localhost:3001",
+    facebook : {
+        appId : "1978073672430696",
+        provider: "facebook"
+    }
+}

b55e93af1b67a3d341fd260ebf13c43a74453c39
Quentin Sahal <quentin.sahal@gmail.com>
2019-05-13 13:59:11 +0200

    small refactoring

--- src/config/server.js
+++ src/config/server.js
@@ -9,6 +9,7 @@ const {
 export default {
     url: `${REACT_APP_API_PROTOCOL}://${REACT_APP_API_DOMAIN}:${REACT_APP_API_PORT}`,
     facebook : {
-        appId : REACT_APP_API_FB_APPID,        provider: "facebook"
+        appId : REACT_APP_API_FB_APPID,        
+        provider: "facebook"
     }
 }
